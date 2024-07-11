87b0d0190d6980a6020b2968a8b987a552dda856
Gopi Prakasam <gopikrishnan.prakasam@kone.com>
2024-07-11 09:57:47 +0530

    first commit

---
+++ basics1.go
@@ -0,0 +1,126 @@
+// // // package main
+
+// // // import "fmt"
+
+// // // func main() {
+// // // 	fmt.Println("Hello, World!")
+// // // }
+// // package main
+
+// // import (
+// //     "fmt"
+// //     "io/ioutil"
+// //     "net/http"
+// // )
+
+// // func main() {
+// //     // Create a new HTTP client
+// //     client := &http.Client{}
+
+// //     // Create a new HTTP request
+// //     req, err := http.NewRequest("GET", "https://example.com", nil)
+// //     if err != nil {
+// //         fmt.Println(err)
+// //         return
+// //     }
+
+// //     // Add headers if needed
+// //     req.Header.Add("Accept", "application/json")
+
+// //     // Send the request
+// //     resp, err := client.Do(req)
+// //     if err != nil {
+// //         fmt.Println(err)
+// //         return
+// //     }
+// //     defer resp.Body.Close()
+
+// //     // Read the response body
+// //     body, err := ioutil.ReadAll(resp.Body)
+// //     if err != nil {
+// //         fmt.Println(err)
+// //         return
+// //     }
+
+// //     // Print the response body
+// //     fmt.Println(string(body))
+// // }
+// package main
+
+// import (
+//     "encoding/json"
+//     "net/http"
+//     "time"
+// )
+
+// type TimeRequest struct {
+//     Time     string `json:"time"`
+//     Duration string `json:"duration"`
+// }
+
+// type TimeResponse struct {
+//     Result time.Time `json:"result"`
+// }
+
+// func timeHandler(w http.ResponseWriter, r *http.Request) {
+//     var req TimeRequest
+//     err := json.NewDecoder(r.Body).Decode(&req)
+//     if err != nil {
+//         http.Error(w, err.Error(), http.StatusBadRequest)
+//         return
+//     }
+
+//     t, err := time.Parse(time.RFC3339, req.Time)
+//     if err != nil {
+//         http.Error(w, err.Error(), http.StatusBadRequest)
+//         return
+//     }
+
+//     d, err := time.ParseDuration(req.Duration)
+//     if err != nil {
+//         http.Error(w, err.Error(), http.StatusBadRequest)
+//         return
+//     }
+
+//     result := t.Add(d)
+
+//     resp := TimeResponse{Result: result}
+//     json.NewEncoder(w).Encode(resp)
+// }
+
+// func main() {
+//     http.HandleFunc("/time", timeHandler)
+//     http.ListenAndServe(":8080", nil)
+// }
+// // 
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "time"
+)
+
+type Response struct {
+    Message string `json:"message"`
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    currentTime := time.Now()
+    formattedTime := currentTime.Format("2006-01-02 15:04:05") 
+    response := Response{
+        Message: "Hello! " + formattedTime,
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+    http.HandleFunc("/", handleRequest)
+    http.ListenAndServe(":8080", nil)
+}
